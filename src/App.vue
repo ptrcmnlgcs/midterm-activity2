2bdb73c81e1626fcf07620cb2dcadecf94287643
angellyn <guardianangellyn65@gmail.com>
2024-04-14 18:00:08 +0800

    add codes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-  </nav>
-  <router-view/>
+  <div id="app">
+   
+    <BiscuitPage />
+  </div>
 </template>
+
+<script>
+import BiscuitPage from './components/BiscuitPage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    BiscuitPage
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+.h1{
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
