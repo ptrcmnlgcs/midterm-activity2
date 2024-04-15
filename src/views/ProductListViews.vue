7f4ca9be99954d903d20f44668a5dcd9322a6937
angellyn <guardianangellyn65@gmail.com>
2024-04-16 07:28:19 +0800

    Created file for product list views

---
+++ src/views/ProductListViews.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- Use the ProductList component here -->
+        <ProductList />
+    </div>
+</template>
+
+<script>
+// Import the ProductList component
+import ProductList from '@/components/ProductList.vue';
+
+export default {
+    name: 'ProductListView',
+    components: {
+        ProductList
+    }
+}
+</script>
