382bd083ed2e1fa41e3ec03fdefa21610f91ac13
angellyn <guardianangellyn65@gmail.com>
2024-04-16 07:27:43 +0800

    Created a file for Biscuit Page View

---
+++ src/views/BiscuitPageViews.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <BiscuitPage />
+    </div>
+</template>
+
+<script>
+
+import BiscuitPage from '@/components/BiscuitPage.vue'
+
+export default {
+    name: 'BiscuitPage',
+    components: {
+        BiscuitPage
+    }
+}
+</script>
