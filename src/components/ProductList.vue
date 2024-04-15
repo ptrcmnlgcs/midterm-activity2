31115f2cca69e535947bf81e25eea17e3a7eea49
angellyn <guardianangellyn65@gmail.com>
2024-04-16 07:25:59 +0800

    add edit and delete buttons

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,46 +1,57 @@
 <template>
-    <div class="container">
-      <h1>BISCUITS</h1>
-      <table class="product-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Action</th> <!-- Added action column -->
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(biscuit, index) in biscuits" :key="index">
-            <td>{{ biscuit.name }}</td>
-            <td>{{ biscuit.description }}</td>
-            <td>${{ biscuit.price }}</td>
-            <td>
-              <!-- <button @click="addToCart(biscuit)">Add </button> Example action button -->
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ProductList',
-    props: {
-      biscuits: {
-        type: Array,
-        required: true
-      }
-    },
-    methods: {
-      addToCart(biscuit) {
-        // Implement your addToCart logic here
-        console.log('Added', biscuit.name, 'to cart');
-      }
+  <div class="container">
+    <h1>BISCUITS</h1>
+    <table class="product-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Action</th> <!-- Added action column -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(biscuit, index) in biscuits" :key="index">
+          <td>{{ biscuit.name }}</td>
+          <td>{{ biscuit.description }}</td>
+          <td>${{ biscuit.price }}</td>
+          <td>
+            <button @click="editBiscuit(biscuit)">Edit</button>
+            <button @click="deleteBiscuit(biscuit)">Delete</button>
+            <!-- <button @click="addToCart(biscuit)">Add </button> Example action button -->
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    biscuits: {
+      type: Array,
+      required: true
     }
-  };
-  </script>
+  },
+  methods: {
+    addToCart(biscuit) {
+      // Implement your addToCart logic here
+      console.log('Added', biscuit.name, 'to cart');
+    },
+    editBiscuit(biscuit) {
+      // Implement your editBiscuit logic here
+      console.log('Editing', biscuit.name);
+    },
+    deleteBiscuit(biscuit) {
+      // Implement your deleteBiscuit logic here
+      console.log('Deleting', biscuit.name);
+    }
+  }
+};
+</script>
+
   
   <style scoped>
   .container {
@@ -81,6 +92,21 @@
     background-color: #f2f2f2;
     text-align: left;
   }
+
+
+.product-table button {
+  padding: 8px 16px; 
+  border: none; 
+  border-radius: 4px; 
+  cursor: pointer; 
+  margin-right: 8px; 
+}
+
+/* Style for the last button in the row to remove right margin */
+.product-table button:last-child {
+  margin-right: 0;
+}
+
 
   </style>
   
